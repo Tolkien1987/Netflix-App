0def604f8e30c1c14c0227d452ac32e9ca30e5a6
Fabio Martinez <fabiomartinez@MacBook-Pro-de-Fabio.local>
2023-01-12 21:17:09 -0300

    Add tabBar

---
+++ Netflix/Netflix/Controllers/UpcomingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UpcomingViewController.swift
+//  Netflix
+//
+//  Created by Fabio Martinez on 12/01/23.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+}
